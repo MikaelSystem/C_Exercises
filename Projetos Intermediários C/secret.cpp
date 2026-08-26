8129d51352071040df9947dd33f1238cbf7d1711
Mikael Oliveira <Mikaelroblox076@gmail.com>
2026-08-26 12:54:35 -0300

    Update secret.cpp

--- Projetos Intermediários C/secret.cpp
+++ Projetos Intermediários C/secret.cpp
@@ -14,6 +14,8 @@ int main(){
     InitWindow(800,600,"Fnaf");
     SetTargetFPS(60);
 
+    int chance_mover=0;
+
     animatronic freddy;
         freddy.sala_atual = -1;
     animatronic bonnie;
@@ -23,7 +25,7 @@ int main(){
     animatronic foxy;
         foxy.sala_atual = -3;
 
-    while (!WindowShouldClose){
+    while (!WindowShouldClose()){
 
         float tempo_relogio = GetFrameTime();
         freddy.cronometro += tempo_relogio;
@@ -33,34 +35,112 @@ int main(){
 
         if (freddy.cronometro >= freddy.intervalo){
             if (GetRandomValue(1,20) <= freddy.nivel_ia){
-                 = GetRandomValue(0,2)
-                if (freddy.sala)
-                
+
+                if (freddy.sala_atual <= 3) {
+                    freddy.sala_atual = GetRandomValue(1,7);
+                }
+                else if (freddy.sala_atual >=4 && freddy.sala_atual <=7){
+                    freddy.sala_atual = GetRandomValue(8,11);
+                }
+                else { 
+                    chance_mover = GetRandomValue(0,20);  //está nos corredores
+
+                    if (chance_mover <=4){
+                        freddy.sala_atual = GetRandomValue(4,6); //Volta
+                    }
+                    else if (chance_mover >=5 && chance_mover <=10){
+                        freddy.sala_atual=freddy.sala_atual; //Fica parado
+                    }
+                    else {
+                        if (freddy.sala_atual == 8 || freddy.sala_atual == 9){
+                            freddy.sala_atual = 12; //Porta do escritório
+                        }
+                        else {
+                            freddy.sala_atual = 13; //Outra porta do escritório
+                        }
+                    }
+                }
             }
 
             freddy.cronometro = 0.0f;
         }
         if (bonnie.cronometro >= bonnie.intervalo){
-            //RNG dele
+
+            if (GetRandomValue(1,20) <= bonnie.nivel_ia){
+
+                if (bonnie.sala_atual <= 3) {
+                    bonnie.sala_atual = GetRandomValue(1,7);
+                }
+                else if (bonnie.sala_atual >=4 && bonnie.sala_atual <=7){
+                    bonnie.sala_atual = GetRandomValue(8,11);
+                }
+                else { 
+                    chance_mover = GetRandomValue(0,20);  //está nos corredores
+
+                    if (chance_mover <=4){
+                        bonnie.sala_atual = GetRandomValue(4,6); //Volta
+                    }
+                    else if (chance_mover >=5 && chance_mover <=10){
+                        bonnie.sala_atual=bonnie.sala_atual; //Fica parado
+                    }
+                    else {
+                        if (bonnie.sala_atual == 8 || bonnie.sala_atual == 9){
+                            bonnie.sala_atual = 12; //Porta do escritório
+                        }
+                        else {
+                            bonnie.sala_atual = 13; //Outra porta do escritório
+                        }
+                    }
+                }
+            }
+
             bonnie.cronometro = 0.0f;
         }
         if (chica.cronometro >= chica.intervalo){
-            //RNG dele
+            
+            if (GetRandomValue(1,20) <= chica.nivel_ia){
+
+                if (chica.sala_atual <= 3) {
+                    chica.sala_atual = GetRandomValue(1,7);
+                }
+                else if (chica.sala_atual >=4 && chica.sala_atual <=7){
+                    chica.sala_atual = GetRandomValue(8,11);
+                }
+                else { 
+                    chance_mover = GetRandomValue(0,20);  //está nos corredores
+
+                    if (chance_mover <=4){
+                        chica.sala_atual = GetRandomValue(4,6); //Volta
+                    }
+                    else if (chance_mover >=5 && chance_mover <=10){
+                        chica.sala_atual=chica.sala_atual; //Fica parado
+                    }
+                    else {
+                        if (chica.sala_atual == 8 || chica.sala_atual == 9){
+                            chica.sala_atual = 12; //Porta do escritório
+                        }
+                        else {
+                            chica.sala_atual = 13; //Outra porta do escritório
+                        }
+                    }
+                }
+            }
+
             chica.cronometro = 0.0f;
         }
         if (foxy.cronometro >= foxy.intervalo){
             //RNG dele
             foxy.cronometro = 0.0f;
         }
-    }
 
-    BeginDrawing();
-        ClearBackground(BLACK);
+        BeginDrawing();
+            ClearBackground(BLACK);
     
         //Desenhar os animatronics na camera
         //Desenhar as texturas DrawTexture()
 
-    EndDrawing();
+        EndDrawing();
+    }
 
     UnloadTexture();
 
